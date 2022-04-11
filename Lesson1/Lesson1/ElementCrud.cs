aac8652b46f99a495d81235aafa0fca37d4d038b
VladimirDolguev <avus93@mail.ru>
2022-04-11 18:35:44 +0300

    Урок 1 Часть 4
    Создайте класс-обертку над List<T>, что бы можно было добавлять и удалять элементы из
    разных потоков без ошибок

---
+++ Lesson1/Lesson1/ElementCrud.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Lesson1;
+
+internal class ElementCrud<T> : List<T> where T : class
+{
+    private readonly object _lock = new();
+
+    public new void Add(T item)
+    {
+        lock (_lock)
+            base.Add(item);
+    }
+
+    public new void Clear()
+    {
+        lock (_lock)
+            base.Clear();
+    }
+
+    public new bool Remove(T item)
+    {
+        lock (_lock)
+            return base.Remove(item);
+    }
+}
